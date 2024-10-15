e2b5c60728705885da961d368131c9a7b9219fb0
Dawood <Dawood@DaHoodLaaitie>
2024-10-15 21:09:08 +0200

    Second Commit

---
+++ src/main/java/com/mycompany/icetask_6/IceTask_6.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask_6;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dawood
+ */
+public class IceTask_6 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+        int vowelCount = countVowels(sentence);
+        System.out.println("The number of vowels in the sentence is: " + vowelCount);
+
+    }
+}
